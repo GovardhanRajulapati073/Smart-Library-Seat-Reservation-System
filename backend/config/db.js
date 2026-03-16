bb2243582d60cd0ed7857f26b46970b6090ddc67
Govardhan <2303A54044@sru.edu.in>
2026-03-16 23:29:23 +0530

    updated database config for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "smart_library",
-  password: "1234",
-  port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
